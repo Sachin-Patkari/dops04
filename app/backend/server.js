41a5e41661f5a0146a8c185268435f66936dc8fb
sachin-patkari <sachinx2003x@gmail.com>
2025-11-12 12:49:00 +0530

    Fix backend bind address to 0.0.0.0

--- app/backend/server.js
+++ app/backend/server.js
@@ -24,7 +24,7 @@ const PORT = process.env.PORT || 5000;
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
-    app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+    app.listen(PORT, '0.0.0.0',() => console.log(`Server running on ${PORT}`));
   })
   .catch((err) => {
     console.error('MongoDB connection error:', err.message);
